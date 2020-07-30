feaece4dc310e7be25d70a211bc0012b3604b90d
Tweekes <tmweekes1@gmail.com>
2020-07-30 06:48:18 -0400

    consolidated ctf-scripts repo and improved setup script

--- setup.sh
+++ setup.sh
@@ -16,16 +16,20 @@ if [ $NEEDS_INSTALL = true ] ; then
     exit 1
 fi
 
-PROG_DIRS=$(ls -l | grep ^d | awk '{print $9}')
+# Directories to be excluded from ln setup below.
+# Directories are excluded using a grep pattern so should be separated by '|'.
+EXCL_DIRS="scripts" 
+
+PROG_DIRS=$(ls -l | grep -v -E $EXCL_DIRS | grep ^d | awk '{print $9}')
 
 for PROG_DIR in ${PROG_DIRS[@]} ; do 
     FILE=$(ls $PROG_DIR | grep $PROG_DIR)
     
     if test -f "$HOME/.$FILE" ; then
-	echo "Deleting $HOME/.$FILE"
-	ln -sf "$PWD/$PROG_DIR/$FILE" "$HOME/.$FILE"
+		echo "Deleting $HOME/.$FILE"
+		ln -sf "$PWD/$PROG_DIR/$FILE" "$HOME/.$FILE"
     else
-	ln -sf "$PWD/$PROG_DIR/$FILE" "$HOME/.$FILE"
+		ln -sf "$PWD/$PROG_DIR/$FILE" "$HOME/.$FILE"
     fi 
 done
 
